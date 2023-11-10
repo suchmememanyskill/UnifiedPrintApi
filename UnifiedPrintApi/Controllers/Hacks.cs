9ed18e88a222a2823519aba30bb3606a6aa8db10
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2023-11-10 16:49:30 +0100

    Add hacks

---
+++ UnifiedPrintApi/Controllers/Hacks.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnifiedPrintApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Hacks : Controller
+{
+    [HttpGet("prusa")]
+    public IActionResult OpenInPrusa(string url)
+    {
+        string redirect = $"prusaslicer://open?file={HttpUtility.UrlEncode(url)}";
+        Console.WriteLine(redirect);
+        return Redirect(redirect);
+    }
+}
